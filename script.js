fccf981f6f90e46bdb9e10d1cbc65064a2199794
rewqla <desares526@gmail.com>
2023-02-06 14:50:03 +0200

    Update functions for api

--- script.js
+++ script.js
@@ -1,31 +1,23 @@
-const config = {
-    NP_KEY: "8927155094ab3dbc66bcc1dfee991a94"
-}
+const url = "https://api.novaposhta.ua/v2.0/json/";
+const configKEY = "8927155094ab3dbc66bcc1dfee991a94";
 
-//tracking
-const requestBody = {
-    apiKey: config.NP_KEY,
+// DocumentNumber: "59000923176535",
+// DocumentNumber: "59000923177130",
+// DocumentNumber: "59000923175689",
+// DocumentNumber: "59000923176082",
+//Phone: "0676733781"
+
+let requestBody = {
+    apiKey: configKEY,
     modelName: "TrackingDocument",
     calledMethod: "getStatusDocuments",
     methodProperties: {
-        Documents: [
-            { 
-                DocumentNumber:"59000923176082",
-                Phone:"0676733781"
-            }          
-        ]},
-        
-        // Documents: ["59000923176535"],
-        // Documents: ["59000923177130"],
-        // Documents: ["59000923175689"],
-        // Documents: ["59000923176082"],
-    };
+        Documents: []
+    }
+};
 
-
-async function StartWork() {
-    let url = "https://api.novaposhta.ua/v2.0/json/";
-
-    const response = await fetch(url, {
+async function GetResponseResult() {
+    let response = await fetch(url, {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
@@ -33,13 +25,45 @@ async function StartWork() {
         body: JSON.stringify(requestBody),
     });
 
-    let result = await response.json();
-
-    return result.data[0];
+    return await response.json();
 }
 
-function MakeTTNs() {
-    let data = StartWork();
+function FillDocumnetsFromPage() {
+    requestBody.methodProperties.Documents = [];
+    requestBody.methodProperties.Documents.push(GetPageData());
+}
+
+async function FillDocumnetsWithFinalData(){
+    requestBody.methodProperties.Documents[0].Phone=await GetRecipientPhone();
+}
+
+function GetPageData() {   
+
+    return {
+        DocumentNumber: document.getElementById("Number").value,
+        Phone: document.getElementById("Phone").value   
+    }
+}
+
+async function GetRecipientPhone() {
+    return (await GetResponseResult()).data[0].PhoneRecipient;
+}
+
+
+
+async function FillDocumnets(){
+    FillDocumnetsFromPage();
+    await FillDocumnetsWithFinalData();
+}
+
+async function LoadTTNData() {
+    await FillDocumnets();
+    return (await GetResponseResult()).data[0];
+}
+
+function CreateTTNs() {
+    console.log(1);
+    let data = LoadTTNData();
     data.then((a) => {
         console.log(a);
         console.log("number of send " + a.Number);
@@ -47,14 +71,14 @@ function MakeTTNs() {
         console.log("estimated date of arrival of the cargo " + a.ScheduledDeliveryDate);
         console.log("city sender " + a.CitySender);
         console.log("address of the department sender " + a.WarehouseSenderAddress);
-        console.log("?phone of sender "+a.PhoneSender);
-        console.log("?name of sender "+a.SenderFullNameEW);
-        console.log("?description of sender "+a.CounterpartySenderDescription);
+        console.log("?phone of sender " + a.PhoneSender);
+        console.log("?name of sender " + a.SenderFullNameEW);
+        console.log("?description of sender " + a.CounterpartySenderDescription);
         console.log("city recipient " + a.CityRecipient);
         console.log("physical person " + a.CounterpartyType);
         console.log("address of the department sender " + a.WarehouseRecipientAddress);
-        console.log("phone of recipient "+a.PhoneRecipient);
-        console.log("name of recipient "+a.RecipientFullName);
+        console.log("phone of recipient " + a.PhoneRecipient);
+        console.log("name of recipient " + a.RecipientFullName);
         console.log("Number of places " + a.SeatsAmount);
         console.log("Cargo weight " + a.DocumentWeight);
         console.log("Type of cargo " + a.CargoType);
@@ -62,9 +86,36 @@ function MakeTTNs() {
         console.log("Price of delivery " + a.DocumentCost);
         console.log("Payment Method " + a.PaymentMethod);
         console.log("Storage " + a.VolumeWeight);
-        console.log("Description "+a.CargoDescriptionString);
+        console.log("Description " + a.CargoDescriptionString);
         console.log("Near price " + a.AnnouncedPrice);
     });
 }
-MakeTTNs();
 
+let t1="0674325675";
+let t2="+380674325675";
+let t3="0124325675";
+let t4="380674325675";
+let t5="59009231f76082";
+
+//0674325675 - correct
+// let regexPhone=new RegExp("^0[0-9]{9}$");
+
+//+380674325675 - correct
+// let regexPhone=new RegExp("^[+]38(0[0-9]{9})$");
+
+//+380674325675 and 0674325675 - correct when +0674325675 - correct too
+// let regexPhone=new RegExp("[+]?^3?8?(0[0-9]{9})$");
+
+// final version
+// let regexPhone=new RegExp("^([+]?(38))?(0[0-9]{9})$");
+
+//final version 
+//let regexNumber=new RegExp("^[0-9]{14}$");
+
+
+// if(regexNumber.test(t5)){
+//     console.log("correct")
+// }
+// else{
+//     console.log("wrong")
+// }
