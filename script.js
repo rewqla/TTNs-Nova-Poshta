cc4c034cdfdd00d8be26e61a098dac79a6ee38a8
rewqla <desares526@gmail.com>
2023-03-03 12:21:46 +0200

    Add data validation on the form

--- script.js
+++ script.js
@@ -30,9 +30,11 @@ const configKEY = "82553fd1d61bd7d87d24d3a51fa6c0c9";
 //4d5d01a7-b132-11ed-a60f-48df37b921db - отримувач ref контрагент
 //4d3d25aa-b132-11ed-a60f-48df37b921db - відправник ref контрагент
 //"208a60fa-45fc-48ef-98e5-53f0819ebf4f" - контрагент 
+"4d5d01a7-b132-11ed-a60f-48df37b921db"
 
 //Метод для запитів
 async function GetResponseResult(model, method, properties) {
+    //++++
     const requestBody = {
         apiKey: configKEY,
         modelName: model,
@@ -62,6 +64,7 @@ async function GetResponseResult(model, method, properties) {
 
 //Створення ттн 
 async function newInternetDocument(params) {
+    //++++
     console.log(params)
     let internetDocument = await GetResponseResult("InternetDocument", "save", params);
     console.log(internetDocument);
@@ -78,7 +81,7 @@ async function createContactPersonRecipient(firstName, middleName, lastName, pho
         CounterpartyType: "PrivatePerson",
         CounterpartyProperty: "Recipient"
     });
-    //console.log(recipient);
+    console.log(recipient);
 
     return {
         "Recipient": recipient.data[0].Ref,
@@ -108,7 +111,6 @@ async function getWarehouseCity(cityName, address, type) {
     }
 }
 
-
 //Отримання контрагента
 async function getCounterpartyRef(type) {
     //++++
@@ -137,6 +139,7 @@ async function getContactPersons(ref, type) {
 }
 
 function GetPageData() {
+    //++++
     return {
         PayerType: getByIdValue("payerType"),
         PaymentMethod: getByIdValue("paymentMethod"),
@@ -148,23 +151,142 @@ function GetPageData() {
     };
 }
 
+//++++
+const isRequired = value => value === '' ? false : true;
+
+function CheckFieldsIfEmpty() {
+    //++++
+    let inputs = document.querySelectorAll("form input");
+    let isValid = true;
+
+    inputs.forEach(element => {
+        let errorSpan = document.querySelector("span[for=" + element.id + "]");
+        if (!isRequired(element.value)) {
+            errorSpan.textContent = "Поле має бути не пустим";
+            isValid = false;
+        }
+        else {
+            errorSpan.textContent = "";
+        }
+    });
+
+    return isValid;
+}
+
+function checkPhone(typeOfPerson) {
+    //++++
+    let regexPhone = new RegExp("^([+]?(38))?(0[0-9]{9})$");
+    let phone = document.getElementById(typeOfPerson + "Phone");
+    let errorSpan = document.querySelector("span[for=" + typeOfPerson + "Phone" + "]");
+    // console.log(phone)
+    if (regexPhone.test(phone.value)) {
+        errorSpan.textContent = "";
+        return true;
+    }
+
+    errorSpan.textContent = "Не правильний формат телефону";
+    return false;
+}
+
+async function CheckAddress(typeOfPerson) {
+    //++++
+    let city = document.getElementById(typeOfPerson + "City");
+    let address = document.getElementById(typeOfPerson + "Address");
+
+    let warehouses = await GetResponseResult("Address", "getWarehouses", {
+        CityName: city.value
+    });
+    // console.log(warehouses)
+
+    let errorSpan = document.querySelector("span[for=" + typeOfPerson + "City" + "]");
+    if (warehouses.data.length > 0) {
+        errorSpan.textContent = "";
+        errorSpan = document.querySelector("span[for=" + typeOfPerson + "Address" + "]");
+
+        for (let i = 0; i < warehouses.data.length; i++) {
+            if (warehouses.data[i].Description == address.value) {
+                errorSpan.textContent = "";
+                return true;
+            }
+        }
+
+        errorSpan.textContent = "Введіть правильну адрессу відділення";
+        return false;
+    }
+
+    errorSpan.textContent = "Введіть правильне місто";
+    return false;
+}
+
+
+async function CheckRecipientData() {
+    return checkPhone("recipient") && await CheckAddress("recipient");
+}
+
+async function CheckSenderData() {
+    return checkPhone("sender") && await CheckAddress("sender");
+}
+
+function CheckAdditionalData() {
+    //++++
+    let ids = ["weight", "volumeGeneral", "seatsAmount", "cost"];
+    let isValid = true;
+    for (let i = 0; i < ids.length; i++) {
+        let errorSpan = document.querySelector("span[for=" + ids[i] + "]");
+        let inputData = document.getElementById(ids[i]).value * 1;
+
+        if (typeof inputData === 'number' && !Number.isNaN(inputData)) {
+            if (inputData <= 0) {
+                errorSpan.textContent = "Поле має бути більшим за 0";
+                isValid = false;
+            }
+            else {
+                errorSpan.textContent = "";
+            }
+        }
+        else {
+            errorSpan.textContent = "Поле має бути числового типу";
+            isValid = false;
+        }
+    }
+
+    return isValid;
+}
+
 let novaPoshtaForm = document.getElementById("novaPoshtaForm");
 novaPoshtaForm.addEventListener("submit", async function (e) {
-    let params = await createContactPersonRecipient(getByIdValue("recipientName"), getByIdValue("recipientMiddleName"), getByIdValue("recipientLastName"), getByIdValue("recipientPhone"));;
-    Object.assign(params, await getWarehouseCity(getByIdValue("recipientCity"), getByIdValue("recipientAddress"), "Recipient"));
-    Object.assign(params, await getContactPersons(await getCounterpartyRef("Sender"), "Sender"));
-    Object.assign(params, await getWarehouseCity(getByIdValue("senderCity"), getByIdValue("senderAddress"), "Sender"));
-    Object.assign(params, await GetPageData());
+    if (await CheckSenderData() && await CheckRecipientData() && CheckAdditionalData() && CheckFieldsIfEmpty()) {
+        // e.preventDefault();
+        // console.log(2)
+        //no problems
+        let params = await createContactPersonRecipient(getByIdValue("recipientName"), getByIdValue("recipientMiddleName"), getByIdValue("recipientLastName"), getByIdValue("recipientPhone"));;
 
-    params.CargoType = "Parcel";
-    params.ServiceType = "WarehouseWarehouse";
+        Object.assign(params, await getWarehouseCity(getByIdValue("recipientCity"), getByIdValue("recipientAddress"), "Recipient"));
+        Object.assign(params, await getContactPersons(await getCounterpartyRef("Sender"), "Sender"));
+        Object.assign(params, await getWarehouseCity(getByIdValue("senderCity"), getByIdValue("senderAddress"), "Sender"));
+        Object.assign(params, await GetPageData());
 
-    newInternetDocument(params);
+        params.CargoType = "Parcel";
+        params.ServiceType = "WarehouseWarehouse";
+
+        newInternetDocument(params);
+    }
+    // else{
+    //     console.log(1)
+    // }
 });
 
-function printInternetDocument(){
-    
+async function printInternetDocument() {
+    // "https://my.novaposhta.ua/orders/printMarking85x85/orders[]/20450669314819/type/pdf8/apiKey/"+configKEY
+
+    let xz = await GetResponseResult("InternetDocument", "printDocument", {
+        DocumentRefs: "a359e808-b91e-11ed-a60f-48df37b921db",
+        Type: "html_new"
+    });
+    console.log(xz);
 }
+printInternetDocument();
+
 function getByIdValue(Id) {
     return document.getElementById(Id).value;
 }
